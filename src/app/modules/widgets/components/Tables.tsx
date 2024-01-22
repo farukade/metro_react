07c347acd6a43c31f070db1123f8d0b00b5d7663
farukade <farukadekunle@gmail.com>
2024-01-22 13:11:33 +0100

    update on table widget 9

--- src/app/modules/widgets/components/Tables.tsx
+++ src/app/modules/widgets/components/Tables.tsx
@@ -86,7 +86,7 @@ const Tables: FC = () => {
         </div>
         {/* end::Row */}
 
-        <TablesWidget9 className='mb-5 mb-xl-8' />
+        {/* <TablesWidget9 className='mb-5 mb-xl-8' /> */}
 
         <TablesWidget10 className='mb-5 mb-xl-8' />
 
